98b73f01138725caa281d4480e620346dd6c3dbd
Yuzi Li <yuzili@Yuzis-MacBook-Pro.local>
2021-04-15 00:48:39 -0700

    updated learning_php.php; Updated users_admin.php

---
+++ li.yuzi/demo/learning_php.php
@@ -0,0 +1,49 @@
+<? 
+
+//phpinfo()
+//broken
+
+echo "<h1>Hello World</h1>";
+echo "Goodbye World";
+
+// assignment operator
+$a = 5;
+
+// String Interpolation
+echo "<div>I have $a things</div>";
+echo '<div>I have $a things</div>';
+
+// VALUE TYPES
+
+// Number
+// Integer
+$b = 15;
+// Float
+$b = 0.576;
+
+$b = 10;
+
+// String
+$name = "Yerdude";
+$name = 'Hamilton';
+
+// Boolean
+$isOn = true;
+
+// function, class. object
+
+
+
+// Math
+
+
+// Order of Operation
+// PEMDAS
+echo (5 + 2) * 3;
+
+// Concatenation
+echo "<div>b + a = c</div>";
+echo "<div>$b + $a = ($b+$a)</div>";
+ 
+?>
+
